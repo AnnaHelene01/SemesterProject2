00a8962a6ae6cc0e7a10c5534157c7dc5ff39f56
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-11-22 23:35:30 +0100

    connected user to profile side, not complete

--- js/profile.js
+++ js/profile.js
@@ -1,17 +1,16 @@
 //Legge til brukerens navn
-const welcome = localStorage.getItem('username');
+const username = localStorage.getItem('username');
 //console.log("User logged in:", welcome);
-const userName = document.getElementById('user-name');
 
-userName.innerHTML = welcome;
 
 
 // Endpoints
 const APIurl = " https://api.noroff.dev/api/v1";
-const profileEndpoint = `/auction/profiles/${welcome}?_listings=true`; // POST
+const profileEndpoint = `/auction/profiles/${username}?_listings=true`; // POST
 
 const profileUrl = `${APIurl}${profileEndpoint}`;
 
+let collection = [];
 
 async function getMyProfile (url) {
     try {
@@ -27,13 +26,12 @@ async function getMyProfile (url) {
         const response = await fetch(url, options); 
         console.log(response);
         const profile = await response.json()
+        collection = profile
         console.log("Profil: ", profile)
         console.log("Profil > navn: ", profile.name)
         console.log("Profil > epost: ", profile.email)
-        document.getElementById("profile-credits").innerHTML = profile.credits
-        document.getElementById("profile-email").innerHTML = profile.email
-        //console.log("Posts:", profile.posts);
-        listData(list, outElement)
+        console.log("Collection:", collection);
+        listData(collection , outElement)
     } catch(error) {
         console.warn(error);
     }
@@ -44,25 +42,54 @@ getMyProfile(profileUrl);
 
 const outElement = document.getElementById("post-container");
 
+
 //Liste ut mine poster på html siden
 function listData(list, out){
-    //console.log("List: ", list)
+    console.log("List: ", list)
     //console.log("Out: ", out)
     out.innerHTML = "";
     let newDivs = "";
-
-    for (let user of list) {
-console.log("Listing:", user.name);
         newDivs += `
-        <div>
-        <h1>${user.name}</h1
+        <div class="container border p-5">
+        <div class="row">
+            <div class="col-lg-6 text-center">
+                <img src="${list.avatar}" class="img-fluid rounded-circle w-50">
+                <div class="col-md-12 form-group display-6 text-start">
+                    <p id="avatarMsg"></p>
+                    <label for="avatar">Avatar URL</label>
+                    <textarea class="form-control form-control-lg" id="updateAvatar" placeholder="Place a avatar URL"></textarea>
+                    <div class="text-center">
+                        <a href="#"><input type="submit" value="CHANGE AVATAR" class="btn btn-secondary btn-lg px-4 mt-3"></a>
+                    </div>
+                </div>   
+            </div>
+            <div class="col-lg-6">
+                <h2 class="p-3" id="user-name">${list.name}</h2>
+                <div class="d-flex mt-5">
+                    <p class="p-3 display-3 text-success">CREDIT:</p>
+                    <p class="p-3 display-3" id="profile-credits">${list.credits}</p>
+                </div>
+                <div class="d-flex">
+                    <p class="p-3 display-3 text-success">EMAIL:</p>
+                    <p class="p-3 display-3" id="profile-email">${list.email}</p>
+                </div>
+                <div class="d-flex">
+                    <p class="p-3 display-3 text-success">WEBSITE:</p>
+                    <p class="p-3 display-3">www.example.no</p>
+                </div>
+                <div class="d-flex">
+                    <p class="p-3 display-3 text-success">PHONE:</p>
+                    <p class="p-3 display-3">+123 45 678</p>
+                </div>
+            </div>
         </div>
+    </div>
         `;
-    }
+
         out.innerHTML = newDivs;
+    }
        // if(list.length == 0) {
             //console.log("You have no posts yet!");
          //   const noPostMsg = document.getElementById("noPostMsg");
            // noPostMsg.innerHTML = "You have no posts yet!";
         //}
-}
