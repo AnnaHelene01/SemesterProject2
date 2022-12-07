7f71285ad4f416bc37fc4a8d3670d6f80bd4d2b0
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-12-07 13:12:51 +0100

    added credits to nav when accestoken is there

--- js/users.js
+++ js/users.js
@@ -2,6 +2,8 @@ const loginNav = document.getElementById("login-nav");
 const logoutNav = document.getElementById("logout-nav")
 const profileNav = document.getElementById("profile-nav");
 const usersNav = document.getElementById("users-nav");
+const showCredit = document.getElementById("credits");
+const pCredit = document.getElementById("p-credits");
 // Checking if user is logged in
 function isLoggedin() {
    const accessToken = localStorage.getItem("accessToken");
@@ -9,6 +11,8 @@ function isLoggedin() {
        logoutNav.style.display="none";
        profileNav.style.display="none";
        usersNav.style.display="none";
+       showCredit.style.display="none";
+       pCredit.style.display="none";
    }
    else {
        loginNav.style.display="none";
