697f78e2f1462ef91e233dfc6334ed810ae816d2
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-12-12 14:34:25 +0100

    getting carousel at update media gallery

--- js/listing-edit.js
+++ js/listing-edit.js
@@ -124,7 +124,104 @@ function listBids(bids, out) {
 
     const endBidTime = document.getElementById("endBidTime");
     endBidTime.innerHTML = `${ourDate}`
+
+
+    //GET UPDATE DIV IN A PREVIEW DIV - with carousel if media array is more then 1!!!!!
+    let placeholder =
+    "../placeholder.png";
+    let mediaList;
+    let pointers;
+    let sliderBtns;
   
+  // MEDIA GALLERY IF MORE IMAGES THAN 0! Placeholder if 0 url. - carousel linked in readme.md
+  if (bids.media.length <= 0) {
+    sliderBtns = "";
+    pointers = "";
+    mediaList = `<img class="img-fluid" src="${placeholder}" alt="Placeholder image" style="object-fit: cover;">`;
+  } else if (bids.media.length === 1) {
+    sliderBtns = "";
+    pointers = "";
+    mediaList = `
+                <img class="img img-fluid" src="${bids.media[0]}" alt="Placeholder image">
+        `;
+  } else if (bids.media.length > 1) {
+    sliderBtns = `
+                <button class="carousel-control-prev" type="button" data-bs-target="#mediaCont" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#mediaCont" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+    `;
+    mediaList = `
+            <div class="carousel-item active">
+                <img class="img-fluid" src="${bids.media[0]}" alt="Product image 0" style="object-fit: cover;">
+            </div>
+        `;
+    pointers = `
+            <button type="button" data-bs-target="#mediaCont" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 0"></button>
+    `;
+  
+    for (let i = 1; i < bids.media.length; i++) {
+      //console.log(i, auctions.media.length);
+      mediaList += `
+            <div class="carousel-item">
+                <img class="img-fluid" src="${bids.media[i]}" alt="Product image ${i}" style="object-fit: cover;">
+            </div>
+        `;
+      pointers += `
+                <button type="button" data-bs-target="#mediaCont" data-bs-slide-to="${i}" aria-label="Slide ${i}"></button>
+    `;
+    }
+  } 
+  
+  // Preview elements:
+let previewContainer = document.getElementById("preview-container");
+const previewTitle = document.getElementById("preview-title");
+const previewImg = document.getElementById("preview-img");
+const previewDescription = document.getElementById("preview-description");
+
+// Preview of creating auction:
+editTitle.addEventListener("keyup", preview);
+divEditMedia.addEventListener("keyup", preview);
+editContent.addEventListener("keyup", preview)
+
+async function preview() {
+
+  previewContainer.innerHTML = "";
+  previewContainer.innerHTML = `
+                    <div class="col-sm-12" id="singleMedia">
+                        <div id="mediaCont" class="carousel slide" data-ride="carousel" >
+                         <div class="carousel-indicators">
+                            ${pointers}
+                         </div>
+                         <div class="carousel-inner h-100 w-100">
+                            ${mediaList}
+                         </div> 
+                          ${sliderBtns}
+                        </div>
+                    </div>
+                    <div class="card-body d-flex">
+                   </div>
+
+                   <div class="col-sm-12">
+                   <div class="card mt-5">
+                       <div class="card-body">
+                           <h4 class="card-title">${editTitle.value}</h4>
+                           <p id="preview-description">${editContent.value}</p>
+                       </div>
+                       <div class="d-flex">
+                           <img src="/Img/60111.jpg" class="rounded-circle p-2"
+                           height="40" alt="Avatar" loading="lazy" />
+                           <h4 class="p-2"> Seller</h4>
+                       </div>
+                     </div>
+                   </div>
+              </div>
+          `;
+}
 }
 
 
@@ -187,39 +284,10 @@ editBtn.addEventListener("click", () => {
 
 
 
-// Preview elements:
-let previewContainer = document.getElementById("preview-container");
-const previewTitle = document.getElementById("preview-title");
-const previewImg = document.getElementById("preview-img");
-const previewDescription = document.getElementById("preview-description");
 
-// Preview of creating auction:
-editTitle.addEventListener("keyup", preview);
-editMedia.addEventListener("keyup", preview);
-editContent.addEventListener("keyup", preview)
 
-async function preview() {
-  previewContainer.innerHTML = "";
-  previewContainer.innerHTML = `
-                <div class="col-sm-12">
-                  <div class="card mt-5">
-                    <img src="${
-                        editMedia[0].value !== ""
-                          ? editMedia[0].value
-                          : "../placeholder.png"
-                      }" class="card-img-top card-img" alt="..">
-           
-                      <div class="card-body">
-                          <h4 class="card-title">${editTitle.value}</h4>
-                          <p id="preview-description">${editContent.value}</p>
-                      </div>
-                      <div class="d-flex">
-                          <img src="/Img/60111.jpg" class="rounded-circle p-2"
-                          height="40" alt="Avatar" loading="lazy" />
-                          <h4 class="p-2"> Seller</h4>
-                      </div>
-                    </div>
-                  </div>
-             </div>
-  `;
-}
+  //---------------------------------------------------------------------------------------------------------------
+
+
+//console.log(mediaList, auctions.media.length);
+
